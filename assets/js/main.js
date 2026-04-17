a93fcd0055e15fee0726928c98dbe2496b4ce40f
karlinna <karlinayuw@gmail.com>
2026-04-18 00:08:07 +0700

    add header styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,13 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*==== change header styles ====*/
+const blurHeader = () =>{
+   const header = document.getElementById('header')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+   this.scrollY >= 50 ? header.classList.add('blur-header') 
+                      : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
