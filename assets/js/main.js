fb0628457e2dadb7d9d66b67209dedf4f392c74d
karlinna <karlinayuw@gmail.com>
2026-04-21 22:08:17 +0700

    add scroll and message

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,56 @@ const blurHeader = () =>{
                       : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+/*==== email js ====*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    emailjs.sendForm('service_flcd27l', 'template_kjyek59', '#contact-form', 'MAqEGDBkg1DWbJDEK')
+    .then(() =>{        
+        contactMessage.textContent = 'Message sent successfully ✅' 
+
+        setTimeout(() =>{
+            contactMessage.textContent = '' 
+        }, 5000)
+
+        contactForm.reset()
+
+    }, () =>{
+        contactMessage.textContent = 'Message not sent, please try again' 
+    })  
+}
+contactForm.addEventListener('submit', sendEmail)
+
+/*==== scroll up ====*/
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 350
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						     : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==== scroll sections active link ====*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }
+        else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
